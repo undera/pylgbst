0a84d1ea1f2dfb5d26ea09d0f47693256e15157c
Andrey Pohilko <apc4@ya.ru>
2018-09-12 10:22:41 +0300

    Add missing package to setup.py
    addresses issue #12

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(name='pylgbst',
       description='Python library to interact with LEGO Move Hub (from Lego BOOST set)',
-      version='0.8',
+      version='0.9',
       author='Andrey Pokhilko',
       author_email='apc4@ya.ru',
-      packages=['pylgbst'],
+      packages=['pylgbst', "pylgbst.comms"],
       requires=[],
       extras_require={
           'gatt': ["gatt"],
