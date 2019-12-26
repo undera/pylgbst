dbb15d1a4a0c675c4fe6758f756e22d94d989800
Mike C. Fletcher <mcfletch@vrplumber.com>
2019-12-26 16:32:32 -0500

    Remove commentted line, apply black formatting

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-#from distutils.core import setup
 from setuptools import setup
 
-setup(name='pylgbst',
-      description='Python library to interact with LEGO Move Hub (from Lego BOOST set)',
-      version='1.1.1',
-      author='Andrey Pokhilko',
-      author_email='apc4@ya.ru',
-      packages=['pylgbst', "pylgbst.comms"],
-      requires=[],
-      extras_require={
-          # Note that dbus and gi are normally system packages
-          'gatt': ["gatt","dbus","gi"], 
-          'gattlib': ["gattlib"],
-          'pygatt': ["pygatt","pexpect"],
-          'bluepy': ["bluepy"],
-      }
-      )
+setup(
+    name="pylgbst",
+    description="Python library to interact with LEGO Move Hub (from Lego BOOST set)",
+    version="1.1.1",
+    author="Andrey Pokhilko",
+    author_email="apc4@ya.ru",
+    packages=["pylgbst", "pylgbst.comms"],
+    requires=[],
+    extras_require={
+        # Note that dbus and gi are normally system packages
+        "gatt": ["gatt", "dbus", "gi"],
+        "gattlib": ["gattlib"],
+        "pygatt": ["pygatt", "pexpect"],
+        "bluepy": ["bluepy"],
+    },
+)
