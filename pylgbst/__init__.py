cdb94744332fd443027f73a2efb1a16b6105a869
Andrey Pokhilko <apc4@ya.ru>
2020-02-15 19:37:30 +0300

    Dummy commit

--- pylgbst/__init__.py
+++ pylgbst/__init__.py
@@ -69,3 +69,105 @@ def start_debug_server(iface="hci0", port=9090):
         server.start(port)
     finally:
         server.connection.disconnect()
+
+    # testing coverage
+    def test_motor(self):
+        hub = HubMock()
+        motor = EncodedMotor(hub, MoveHub.PORT_D)
+        hub.peripherals[MoveHub.PORT_D] = motor
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.start_power(1.0)
+        self.assertEqual(b"07008103110164", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.stop()
+        self.assertEqual(b"0c0081031109000064647f03", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.set_acc_profile(1.0)
+        self.assertEqual(b"090081031105e80300", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.set_dec_profile(1.0)
+        self.assertEqual(b"090081031106e80300", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.start_speed(1.0)
+        self.assertEqual(b"090081031107646403", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.stop()
+        self.assertEqual(b"0c0081031109000064647f03", hub.writes.pop(1)[1])
+
+        logging.debug("\n\n")
+        hub.connection.notification_delayed('0500820301', 0.1)
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.timed(1.0)
+        self.assertEqual(b"0c0081031109e80364647f03", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('0500820301', 0.1)
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.angled(180)
+        self.assertEqual(b"0e008103110bb400000064647f03", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.preset_encoder(-180)
+        self.assertEqual(b"0b0081031151024cffffff", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('0500820301', 0.1)
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.goto_position(0)
+        self.assertEqual(b"0e008103110d0000000064647f03", hub.writes.pop(1)[1])
+
+        hub.connection.wait_notifications_handled()
+
+        hub = HubMock()
+        motor = EncodedMotor(hub, MoveHub.PORT_D)
+        hub.peripherals[MoveHub.PORT_D] = motor
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.start_power(1.0)
+        self.assertEqual(b"07008103110164", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.stop()
+        self.assertEqual(b"0c0081031109000064647f03", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.set_acc_profile(1.0)
+        self.assertEqual(b"090081031105e80300", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.set_dec_profile(1.0)
+        self.assertEqual(b"090081031106e80300", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.start_speed(1.0)
+        self.assertEqual(b"090081031107646403", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.1)
+        motor.stop()
+        self.assertEqual(b"0c0081031109000064647f03", hub.writes.pop(1)[1])
+
+        logging.debug("\n\n")
+        hub.connection.notification_delayed('0500820301', 0.1)
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.timed(1.0)
+        self.assertEqual(b"0c0081031109e80364647f03", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('0500820301', 0.1)
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.angled(180)
+        self.assertEqual(b"0e008103110bb400000064647f03", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.preset_encoder(-180)
+        self.assertEqual(b"0b0081031151024cffffff", hub.writes.pop(1)[1])
+
+        hub.connection.notification_delayed('0500820301', 0.1)
+        hub.connection.notification_delayed('050082030a', 0.2)
+        motor.goto_position(0)
+        self.assertEqual(b"0e008103110d0000000064647f03", hub.writes.pop(1)[1])
+
+        hub.connection.wait_notifications_handled()
