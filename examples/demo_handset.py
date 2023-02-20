1447a3117888b8775c829d07eca13211cc163435
Ivo <ivobusko@gmail.com>
2023-02-20 10:46:59 -0500

    comment

--- examples/demo_handset.py
+++ examples/demo_handset.py
@@ -7,6 +7,7 @@ remote = RemoteHandset()
 def callback_from_button(button, button_set):
     print("value from callback: ", button, button_set)
 
+# valid modes are 0 (default), 1, and 2. All result in the same behavior.
 remote.port_A.subscribe(callback_from_button, mode=2)
 remote.port_B.subscribe(callback_from_button)
 time.sleep(60)
